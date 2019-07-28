de660bb253717837ca717e9fe05da31d95ad1173
nandugit <nandkishorcse@gmail.com>
2019-07-28 21:49:36 +0530

    First commit to Repo

--- app/src/main/java/com/brodcast/nandkishor/switchingactivity/Main2Activity.java
+++ app/src/main/java/com/brodcast/nandkishor/switchingactivity/Main2Activity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
-    @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
--- app/src/main/java/com/brodcast/nandkishor/switchingactivity/MainActivity.java
+++ app/src/main/java/com/brodcast/nandkishor/switchingactivity/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTextNumber1;
     private EditText mEditTextNumber2;
 
-    @Override
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
